f97df408873d21671d7ad2a8a184d4771d4dde2d
Vanhxyz <79246327+VanhKoi@users.noreply.github.com>
2024-01-17 21:52:22 +0700

    Update autorankpet99.lua
    f

--- autorankpet99.lua
+++ autorankpet99.lua
@@ -1,84 +1,76 @@
- local f = {}
-function checkquestmap1()
-    f={}
-    for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.TeleportMap.Frame.Map.Locations:GetChildren()) do
-        if v:IsA("Frame") and string.find(v.Box.TextHolder.Number.Text, "#") and
-            not string.find(v.Box.TextHolder.Title.Text, "???") then
+ 
 
-            table.insert(f, tonumber(string.match(v.Box.TextHolder.Number.Text, "%d+")))
-        
+local g = {}
+local h = {}
+ function checkquestmap1()
+    g={}
+    for i, v in pairs(workspace.Map:GetChildren()) do
+        if v:IsA("Folder") and v.INTERACT:FindFirstChild("Gate")   then
+            table.insert(g, tonumber(string.match(v.Name, "%d+")))
+
         end
     end
-    table.sort(f, function(a, b)
+    table.sort(g, function(a, b)
+        return a > b
+    end)    
+end
+function checkmap2()
+    h={}
+     for i, v in pairs(workspace.Map:GetChildren()) do
+        if v:IsA("Folder") and not v.INTERACT:FindFirstChild("Gate")     then
+            table.insert(h, tonumber(string.match(v.Name, "%d+")))
+ 
+         
+    end
+    end
+    table.sort(h, function(a, b)
         return a > b
     end)
 end
-checkquestmap1()
- 
-function checkmap()
+  while _G.abcd do wait()
+pcall(function()
+    
+ checkmap2()
+ checkquestmap1()
+ local n1 =     tonumber(math.max(table.unpack(h)))
+  function checkmap3()
     for i, v in pairs(workspace.Map:GetChildren()) do
-        if v:IsA("Folder") and tonumber(string.match(v.Name, "%d+")) == tonumber(math.max(table.unpack(f))) then
+        if v:IsA("Folder") and tonumber(string.match(v.Name, "%d+")) == n1 then
             return v
         end
     end
 end
- 
-while _G.abcd do wait()
-    for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.TeleportMap.Frame.Map.Locations:GetChildren()) do
-        if v:IsA("Frame") and tonumber(string.match(v.Box.TextHolder.Number.Text, "%d+")) ==
-            tonumber(math.max(table.unpack(f))) and not checkmap() then
-            local args = {
-                [1] = v.Box.TextHolder.Title.Text
-            }
-
-            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Teleports_RequestTeleport")
-                :InvokeServer(unpack(args))
-        end
-    end
-    local function click(a)
-        game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X + a.AbsoluteSize.X / 2,
-            a.AbsolutePosition.Y + 50, 0, true, a, 1)
-        game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X + a.AbsoluteSize.X / 2,
-            a.AbsolutePosition.Y + 50, 0, false, a, 1)
-    end
-    -- game:GetService("Players").LocalPlayer.PlayerGui.TeleportMap.Frame.Visible = false 
-    pcall(function()
-        
-  
-    game:GetService("Players").LocalPlayer.PlayerGui.MainLeft.Left.Tools.Teleport.Parent = game.Players.LocalPlayer
-        .PlayerGui.Main
-        .Boosts
-        .UIGridLayout
-        .Parent
-    end)
-    spawn(function  ()
-        
-     if game:GetService("Players").LocalPlayer.PlayerGui.ProgressBars.Main.Progress.Bar:FindFirstChild("GreenGradient") then
-  
-
- click(game:GetService("Players").LocalPlayer.PlayerGui.Main.Boosts.Teleport.ConsoleButton)
-wait(1)
-        game:GetService("Players").LocalPlayer.PlayerGui.TeleportMap.Frame.Visible = false
- wait(1)
-         for i, v in pairs(game.workspace.Map:GetChildren()) do
-            if v:IsA("Folder") and tonumber(math.max(table.unpack(f))) + 1 == tonumber(string.match(v.Name, "%d+")) then
-
-                b = string.gsub(v.Name, tostring(string.match(v.Name, "%d+")) .. " | ", "")
-                local args = {
-                    [1] = b
-                }
-
-                game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Zones_RequestPurchase")
-                    :InvokeServer(unpack(args))
+   local n = tonumber(math.min(table.unpack(g)))-1
+    function checkmap()
+        for i, v in pairs(workspace.Map:GetChildren()) do
+            if v:IsA("Folder") and tonumber(string.match(v.Name, "%d+")) == n then
+                return v
             end
         end
-    else
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = checkmap().INTERACT.BREAK_ZONES.BREAK_ZONE.CFrame
-
     end
-end)
-
-end
-
  
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = checkmap3().INTERACT.BREAK_ZONES.BREAK_ZONE.CFrame
+
+     if game:GetService("Players").LocalPlayer.PlayerGui.ProgressBars.Main.Progress.Bar:FindFirstChild("GreenGradient") then
  
+                for i, v in pairs(game.workspace.Map:GetChildren()) do
+                   if v:IsA("Folder") and tonumber(math.min(table.unpack(g))) == tonumber(string.match(v.Name, "%d+")) then
+       
+                       b = string.gsub(v.Name, tostring(string.match(v.Name, "%d+")) .. " | ", "")
+                       local args = {
+                           [1] = b
+                       }
+       
+                       game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Zones_RequestPurchase")
+                           :InvokeServer(unpack(args))
+                   end
+               end
+           else
+               game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = checkmap().INTERACT.BREAK_ZONES.BREAK_ZONE.CFrame
+       
+           end
+        
+        
+             
+        end)
+    end
